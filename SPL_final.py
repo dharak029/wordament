05fa0eab5fe83e821354b1b5f117bfad99b0317a
dharak029 <dshah27@uncc.edu>
2017-04-09 11:02:38 -0400

    nothing updated

--- SPL_final.py
+++ SPL_final.py
@@ -1,11 +1,12 @@
 import pygame
 import random
 
-white = (255,255,255)
-darkturqoise = ( 3,  54,  73)
+white = (255, 255, 255)
+darkturqoise = (3, 54, 73)
+black = (0, 0, 0)
+
 
 class Tile:
-
     def __init__(self, x, y, image, cover):
         self.image = image
         self.cover = cover
@@ -29,42 +30,50 @@ class Tile:
         # check left button click
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             # check position
-            if self.rect.collidepoint(event.pos):
+            '''if self.rect.collidepoint(event.pos):
                 self.covered = not self.covered
                 if not self.covered:
                     # if uncovered then set +2000ms to cover
-                    self.time_to_cover = pygame.time.get_ticks() + 2000
+                    #self.time_to_cover = pygame.time.get_ticks() + 2000'''
 
-#----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
 
 # init
 
 pygame.init()
 
-screen = pygame.display.set_mode((550,550))
+screen = pygame.display.set_mode((550, 550))
 screen.fill(darkturqoise)
-pygame.draw.rect(screen, white, [100,100,285,285])
+
+
+total_time = 120
+frame_rate = 60
+frame_count = 0
+
+basicfont = pygame.font.Font('freesansbold.ttf', 30)
 
 # create images
 
-#char = random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+# char = random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
 img = pygame.surface.Surface((30, 30))
-img.fill((255,0,0))
+img.fill((255, 0, 0))
 
 cov = pygame.surface.Surface((30, 30))
-cov.fill((0,255,0))
+cov.fill((0, 255, 0))
 
 # create tiles-
 
 tiles = []
-for y in range(3,11):
-    for x in range(3,11):
-        tiles.append( Tile(x*35, y*35, img, cov) )
+for y in range(3, 11):
+    for x in range(3, 11):
+        tiles.append(Tile(x * 35, y * 35, img, cov))
 
 # mainloop
 
 clock = pygame.time.Clock()
+
 running = True
 
 while running:
@@ -74,9 +83,37 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
 
         for x in tiles:
             x.handle_event(event)
+
+
+    # Timer
+
+    total_seconds = total_time - (frame_count // frame_rate)
+
+    if total_seconds < 0:
+        total_seconds = 0
+
+    minutes = total_seconds // 60
+
+    seconds = total_seconds % 60
+
+    timer = "{0:02}:{1:02}".format(minutes, seconds)
+
+    text = basicfont.render(timer, True, black)
+
+    screen.fill(darkturqoise)
+    screen.blit(text, [100, 65])
+
+    frame_count += 1
+
+    clock.tick(frame_rate)
+
+    pygame.draw.rect(screen, white, [100, 100, 285, 285])
 
     # updates
 
@@ -89,9 +126,5 @@ while running:
         x.draw(screen)
 
     pygame.display.flip()
-
-    # clock
-
-    clock.tick(25)
 
 pygame.quit()
