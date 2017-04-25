bc24b750702c71550f193778ffc4043a70659c1b
vaijyant <vaijyant.tomar@gmail.com>
2017-04-25 13:39:46 -0400

    minor changes

--- wordament_testing_vt.py
+++ wordament_testing_vt.py
@@ -4,11 +4,9 @@ from random import randint
 import itertools
 from copy import deepcopy
 
-words = ["python", "jack", "word", "code", "review", "funion",
-         "light", "cat", "camel", "dog"]
+words = ["python", "jack", "word", "code", "review", "light", "cat", "camel", "dog"]
 
 letters = "qwertyuiopasdfghjklzxcvbnm"
-
 
 # color constants
 WHITE = (255, 255, 255)
@@ -37,15 +35,12 @@ Score_Y = 65
 ROW_SIZE = 8
 COL_SIZE = 8
 
+SCORE = 0
+
 grid = [[j for j in range(ROW_SIZE)] for i in range(COL_SIZE)]
 
 
-
 def makeGrid(words, grid, size=[8, 8], attempts=10):
-    '''Run attemptGrid trying attempts number of times.
-    Size contains the height and width of the board.
-    Word is a list of words it should contain.'''
-
     for _ in range(attempts):
         try:
             return attemptGrid(words, grid, size)
@@ -53,96 +48,81 @@ def makeGrid(words, grid, size=[8, 8], attempts=10):
 
             pass
     else:
-        print ("ERROR - Couldn't create valid board")
+        print("ERROR - Couldn't create valid board")
         raise
 
-def attemptGrid(words, grid, size):
-    '''Attempt a grid of letters to be a wordsearch
-    Size contains the height and width of the board.
-    Word is a list of words it should contain.
-    Returns the 2D list grid and a dictionary of the words as keys and
-    lists of their co-ordinates as values.'''
 
-    #Make sure that the board is bigger than even the biggest word
+def attemptGrid(words, grid, size):
+    # Make sure that the board is bigger than even the biggest word
     sizeCap = (size[0] if size[0] >= size[1] else size[1])
     sizeCap -= 1
     if any(len(word) > sizeCap for word in words):
-        print ("ERROR: Too small a grid for supplied words.")
+        print("ERROR: Too small a grid for supplied words.")
         return
 
-
-    #Insert answers and store their locations
+    # Insert answers and store their locations
     answers = {}
     for word in words:
-        grid, answer = insertWord(word,grid)
+        grid, answer = insertWord(word, grid)
         answers[word] = answer
 
-    #Add other characters to fill the empty space
-    for i,j in itertools.product(range(size[1]), range(size[0])):
+    # Add other characters to fill the empty space
+    for i, j in itertools.product(range(size[1]), range(size[0])):
         if grid[i][j].char == ' ':
-            grid[i][j].set_char(letters[randint(0, len(letters)-1)])
+            grid[i][j].set_char(letters[randint(0, len(letters) - 1)])
 
     return grid, answers
 
-def insertWord(word, grid, invalid=None):
-    '''Insert a word into the letter grid
-    'word' will be inserted into the 2D list grid.
-    invalid is either None or a list of coordinates
-    These coordinates are denote starting points that don't work.
-    Returns an updated grid as well as a list of the added word's indices.'''
 
+def insertWord(word, grid, invalid=None):
     height, width = len(grid), len(grid[0])
     length = len(word)
 
-    #Detect whether the word can fit horizontally or vertically.
+    # Detect whether the word can fit horizontally or vertically.
     hori = width >= length + 1
     vert = height >= length + 1
     if hori and vert:
-        #If both can be true, flip a coin to decide which it will be
-        hori = bool(randint(0,1))
+        # If both can be true, flip a coin to decide which it will be
+        hori = bool(randint(0, 1))
         vert = not hori
 
-    line = [] #For storing the letters' locations
+    line = []  # For storing the letters' locations
     if invalid is None:
-        invalid = [[None,None,True],[None,None,False]]
+        invalid = [[None, None, True], [None, None, False]]
 
-    #Height * width is an approximation of how many attempts we need
-    for _ in range(height*width):
+    # Height * width is an approximation of how many attempts we need
+    for _ in range(height * width):
         if hori:
-            x = randint(0,width-1-length)
-            y = randint(0,height-1)
+            x = randint(0, width - 1 - length)
+            y = randint(0, height - 1)
         else:
-            x = randint(0,width-1)
-            y = randint(0,height-1-length)
-        if [y,x,hori] not in invalid:
+            x = randint(0, width - 1)
+            y = randint(0, height - 1 - length)
+        if [y, x, hori] not in invalid:
             break
     else:
         # Probably painted into a corner, raise an error to retry.
-        raise(RuntimeError)
+        raise (RuntimeError)
 
-    start = [y, x, hori] #Saved in case of invalid placement
-    #Now attempt to insert each letter
+    start = [y, x, hori]  # Saved in case of invalid placement
+    # Now attempt to insert each letter
     for letter in word:
         if grid[y][x].get_char() in (' ', letter):
-            line.append([y,x])
+            line.append([y, x])
             if hori:
                 x += 1
             else:
                 y += 1
         else:
-            #We found a place the word can't fit
-            #Mark the starting point as invalid
+            # We found a place the word can't fit
+            # Mark the starting point as invalid
             invalid.append(start)
             return insertWord(word, grid, invalid)
 
-    #Since it's a valid place, write to the grid and return
-    for i,cell in enumerate(line):
+    # Since it's a valid place, write to the grid and return
+    for i, cell in enumerate(line):
         grid[cell[0]][cell[1]].set_char(word[i])
     return grid, line
-
-# ===copied code ends===================================================================================================
-
-
 
 
 class Tile:
@@ -156,8 +136,8 @@ class Tile:
         self.active = False
         self.time_to_cover = None
         self.screen = screen
-        self.row = y/35 - 3
-        self.col = x/35 - 3
+        self.row = y / 35 - 3
+        self.col = x / 35 - 3
 
     def draw(self):
         self.surf.fill(GREEN)
@@ -178,9 +158,10 @@ class Tile:
         # check left button click
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             # check position
-            if self.rect.collidepoint(event.pos):
-                self.active = not self.active
-                self.update(self.active)
+            self.active = not self.active
+            #if self.rect.collidepoint(event.pos):
+
+            self.update(self.active)
         return self.active, self.char
 
     def set_char(self, char):
@@ -190,8 +171,6 @@ class Tile:
 
     def get_char(self):
         return self.char
-
-# ----------------------------------------------------------------------
 
 def main():
     global BASICFONT
@@ -210,6 +189,7 @@ def main():
         showTitleScreen(screen, "GAME OVER")
     pygame.quit()
 
+
 def showTitleScreen(screen, text):
     keyPressMessageFont = pygame.font.SysFont("Britannic Bold", 24)
 
@@ -221,6 +201,7 @@ def showTitleScreen(screen, text):
             shadowFont = pygame.font.SysFont("Britannic Bold", 48)
             title = font.render(text, 1, WHITE)
             shadow = shadowFont.render(text, 1, GRAY)
+
 
             keyPressMessage = keyPressMessageFont.render("Press any [ key ] to continue...", 1, WHITE)
 
@@ -236,12 +217,13 @@ def showTitleScreen(screen, text):
             shadowFont = pygame.font.SysFont("Britannic Bold", 48)
             title = font.render(text, 1, WHITE)
             shadow = shadowFont.render(text, 1, GRAY)
-
+            scoreText = keyPressMessageFont.render("Your Score: " + str(SCORE), 1, WHITE)
             keyPressMessage = keyPressMessageFont.render("Press any [ key ] to end.", 1, WHITE)
 
             screen.blit(shadow, (152, 102))
             screen.blit(title, (150, 100))
-            screen.blit(keyPressMessage, (155, 300))
+            screen.blit(scoreText, (200, 200))
+            screen.blit(keyPressMessage, (160, 300))
             pygame.display.flip()
             for event in pygame.event.get():
                 if event.type == pygame.KEYUP or event.type == pygame.QUIT:
@@ -258,6 +240,10 @@ def submitButton(screen):
     rect = pygame.Rect(Submit_X, Submit_Y, submitbutton.get_width() + 5, submitbutton.get_height() - 5)
     surf = pygame.surface.Surface(rect.size)
     surf.fill(GREEN)
+
+    pygame.draw.rect(screen, BLACK,
+                     [Submit_X + 3, Submit_Y + 3, submitbutton.get_width() + 5, submitbutton.get_height() - 5])
+
     screen.blit(surf, rect)
     screen.blit(submitbutton, (Submit_X, Submit_Y))
     return rect
@@ -270,6 +256,7 @@ def resetButton(screen):
     rect = pygame.Rect(Reset_X, Reset_Y, submitbutton.get_width() + 5, submitbutton.get_height() - 5)
     surf = pygame.surface.Surface(rect.size)
     surf.fill(GREEN)
+    pygame.draw.rect(screen, BLACK, [Reset_X + 3, Reset_Y + 3, submitbutton.get_width() + 5, submitbutton.get_height() - 5])
     screen.blit(surf, rect)
     screen.blit(submitbutton, (Reset_X, Reset_Y))
     return rect
@@ -277,18 +264,19 @@ def resetButton(screen):
 
 # creating scorelabel
 def scoreLabel(screen, score):
+    global SCORE
     text = 'Score:' + score
+    SCORE = score
     basicfont = pygame.font.Font('freesansbold.ttf', 30)
     scorelabel = basicfont.render(text, True, WHITE)
     rect = pygame.Rect(Score_X, Score_Y, scorelabel.get_width() + 5, scorelabel.get_height() - 5)
     surf = pygame.surface.Surface(rect.size)
-    surf.fill(GREEN)
+    surf.fill(DARKTURQOISE)
     screen.blit(surf, rect)
     screen.blit(scorelabel, (Score_X, Score_Y))
 
 
 def createTiles(screen):
-
     # create
     for row in range(ROW_SIZE):
         for col in range(COL_SIZE):
@@ -300,7 +288,6 @@ def createTiles(screen):
 
     # set characters
     display_grid, answers = makeGrid(words, grid)
-
 
     # draws
     for of_row in display_grid:
@@ -320,12 +307,13 @@ def getTile(grid, event):
 def checkAlignment(lastTiles, currTile):
     horizontal = None
     prevTile = lastTiles[0]
-    if prevTile.row == currTile.row and prevTile.col+1 == currTile.col:
+    if prevTile.row == currTile.row and prevTile.col + 1 == currTile.col:
         horizontal = True
-    if prevTile.col == currTile.col and prevTile.row+1 == currTile.row:
+    if prevTile.col == currTile.col and prevTile.row + 1 == currTile.row:
         horizontal = False
 
     return horizontal
+
 
 def runClock(screen, clock):
     global FRAME_COUNT
@@ -343,7 +331,6 @@ def runClock(screen, clock):
 
     timeLabel = basicfont.render(timer, True, WHITE)
     rect = pygame.Rect(TIMER_X, TIMER_Y, timeLabel.get_width() + 5, timeLabel.get_height())
-    # del. CAUTION! Update the constants in header
     surf = pygame.surface.Surface(rect.size)
 
     surf.fill(DARKTURQOISE)
@@ -371,8 +358,6 @@ def startGame(screen, grid):
 
         runningClock = runClock(screen, clock)
         if runningClock is not None and not runningClock:
-            # del. for now it closes the window, code to be written
-            # del. current total time is set to 3 sec for debug purpouse check TOTAL_TIME on line 13
             return pygame.K_ESCAPE
 
         # events thread
@@ -402,18 +387,17 @@ def startGame(screen, grid):
                     else:  # length > 2
                         if horizontal:
                             lastTile = prevTiles[-1]
-                            if lastTile.row == tile.row and lastTile.col+1 == tile.col:
+                            if lastTile.row == tile.row and lastTile.col + 1 == tile.col:
                                 tile.updateState(event)
                                 prevTiles.append(tile)
                                 word = word + "" + tile.char
                         if not horizontal:
                             lastTile = prevTiles[-1]
-                            if lastTile.col == tile.col and lastTile.row+1 == tile.row:
+                            if lastTile.col == tile.col and lastTile.row + 1 == tile.row:
                                 tile.updateState(event)
                                 prevTiles.append(tile)
                                 word = word + "" + tile.char
 
-                    print(word)  # del. check console when you execute this
                 if reset.collidepoint(event.pos):
                     screen = pygame.display.set_mode((500, 500))
                     screen.fill(DARKTURQOISE)
@@ -427,17 +411,17 @@ def startGame(screen, grid):
                     FRAME_COUNT = 0
                     runClock(screen, clock)
                 if submit.collidepoint(event.pos):
+
                     for x in words:
                         if (word == x and word not in found_words):
                             score = score + 1
                             scoreLabel(screen, str(score))
                             found_words.append(word)
-
+                    else:
+                        for wrong_tile in prevTiles:
+                            wrong_tile.updateState(event)
                     word = ""
-                    for tile in prevTiles:
-                        tile.updateState(event)
                     prevTiles = []
-
 
 
 # calling main()
